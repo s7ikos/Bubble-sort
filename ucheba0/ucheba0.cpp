3f12ef173a04e2c93e704d04cd61e1bff0351a38
s7ikos <mishanya.ktokto@gmail.com>
2024-02-14 18:33:01 +0300

    master

---
+++ ucheba0/ucheba0.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main() {
+	std::vector < int > data = { 3, 5, 2, 7, 8, 1, 5, 11, 23, 4 };
+	for (size_t j = 0; j != data.size() - 1; ++j) {
+		bool flag = false;
+		for (size_t i = 0; i != data.size() - j - 1; ++i) {
+			if (data[i] > data[i + 1]) {
+				int swap = data[i + 1];
+				data[i + 1] = data[i];
+				data[i] = swap;
+				flag = true;
+			}
+			else {
+				continue;
+			}
+		}
+		if (!flag) {
+			break;
+		}
+	}
+	for (const int& elem : data) {
+		std::cout << elem << " ";
+	}
+}
